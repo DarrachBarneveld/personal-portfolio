b9b60bbf2ce26d0056afc43364f6cf8246bafd0a
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-11-04 03:58:43 +0000

    Fixed framer motion heading issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,17 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import clsx from "clsx";
 import { child, container } from "@/animations/nav";
 import { links } from "@/lib/data";
 
 interface NavbarProps {}
 
 const Navbar: FunctionComponent<NavbarProps> = () => {
+  const [activeSection, setActiveSection] = useState("Home");
+
   return (
-    <nav>
+    <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
       <motion.ul
         className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5"
         variants={container}
@@ -22,10 +25,27 @@ const Navbar: FunctionComponent<NavbarProps> = () => {
             variants={child}
           >
             <Link
-              className="flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300"
+              className={clsx(
+                "flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300",
+                {
+                  "text-gray-950": activeSection === link.name,
+                },
+              )}
               href={link.hash}
+              onClick={() => setActiveSection(link.name)}
             >
               {link.name}
+              {link.name === activeSection && (
+                <motion.span
+                  className="absolute inset-0 -z-10 rounded-full bg-violet-100"
+                  layoutId="activeSection"
+                  transition={{
+                    type: "spring",
+                    stiffness: 380,
+                    damping: 30,
+                  }}
+                ></motion.span>
+              )}
             </Link>
           </motion.li>
         ))}
