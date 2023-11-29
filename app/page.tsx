efff7bf9e7cfbd1fc7e3de7384df90de87473d4a
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-11-29 23:56:42 +0000

    Testing push

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
-import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import SectionDivider from "@/components/ui/SectionDivider";
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <About />
       <SectionDivider />
       <Projects />
       <Skills />
