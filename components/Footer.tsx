193236c23c70608ccaf2a4cdc28b1a2fd01f5947
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-12-05 11:22:47 +0000

    Updated footer information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,29 @@ import { FunctionComponent } from "react";
 
 const Footer: FunctionComponent = () => {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
+    <footer className="absolute bottom-0 w-screen bg-[#dbd7fb] bg-opacity-50	 px-4 py-5 text-center text-gray-500 backdrop-blur-xl">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2030 Darrach. All rights reserved.
       </small>
+      <address className="mb-2 text-xs ">
+        <p>
+          <strong>Email:</strong>{" "}
+          <a
+            className="hover:text-sky-500"
+            href="mailto:darrachb1991@gmail.com"
+          >
+            darrachb1991@gmail.com
+          </a>{" "}
+          | <strong>Phone:</strong>{" "}
+          <a className="hover:text-sky-500" href="tel:(+353) 1 87 174 8347">
+            (+353) 1 87 174 8347
+          </a>
+        </p>
+      </address>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        Framer Motion Vercel hosting.
       </p>
     </footer>
   );
