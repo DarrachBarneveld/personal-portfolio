b5d6b35355a6cbf9297daf4db826e51f7ad977b4
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-11 11:56:24 +0000

    Update current year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from "react";
 
 const Footer: FunctionComponent = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bottom-0 w-screen bg-[#dbd7fb] bg-opacity-50	 px-4 py-5 text-center text-gray-500 backdrop-blur-xl">
       <small className="mb-2 block text-sm">
-        &copy; 2030 Darrach. All rights reserved.
+        &copy; {currentYear} Darrach. All rights reserved.
       </small>
       <address className="mb-2 text-sm">
         <p>
@@ -16,8 +17,8 @@ const Footer: FunctionComponent = () => {
             darrachb1991@gmail.com
           </a>{" "}
           | <strong>Phone:</strong>{" "}
-          <a className="hover:text-sky-500" href="tel:(+353) 1 87 174 8347">
-            (+353) 1 87 174 8347
+          <a className="hover:text-sky-500" href="tel:(+353) 87 174 8347">
+            (+353) 87 174 8347
           </a>
         </p>
       </address>
